eb803650037912db5322481fc2e94ccbeb908e14
Muhammad Ali <=>
2019-04-18 08:02:06 -0700

    Navigation Applied

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import {createStackNavigator, createAppContainer} from "react-navigation";
 import Splash from './src/screens/Splash';
 
 
-export default class App extends Component {
+ class App extends Component {
   render() {
     return (
       <View style={styles.contianer}>
-        <Splash/>
+      
       </View>
         );
   }
 }
+
+const AppNavigator = createStackNavigator ({
+      Start: {
+        screen: Splash
+      }
+})
+ export default createAppContainer(AppNavigator)
 
 const styles = {
   contianer: {
