9b7f66df49c339a44e3292de0b7fdec60cd1ffdf
= <=>
2019-04-24 07:51:20 -0700

    Reformated App.js

--- App.js
+++ App.js
@@ -1,24 +1,19 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import {createStackNavigator, createAppContainer} from "react-navigation";
-import Splash from './src/screens/Splash';
+import {View} from 'react-native';
 import Home from './src/screens/Home'
 
-
 export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Home/>
-      </View>
+    render() {
+        return (
+            <View style={styles.container}>
+                <Home/>
+            </View>
         );
-  }
+    }
 }
 
-
-
 const styles = {
-  container: {
-    flex: 1
-  }
+    container: {
+        flex: 1
+    }
 };
